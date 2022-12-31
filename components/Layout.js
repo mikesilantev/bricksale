4afa980ebac28f0c42ab457e25d267d1078492f7
mikesilantev <mike.amigofish@yandex.ru>
2022-12-31 08:49:33 +0300

    first

---
+++ components/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Layout() {
+  return (
+    <>
+      <div>
+        <header>header</header>
+        <main>main</main>
+        <footer>footer</footer>
+      </div>
+    </>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,tx,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
