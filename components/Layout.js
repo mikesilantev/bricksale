8c7f60974d7f3088e1bd7882e36d4f2ed15347b0
mikesilantev <mike.amigofish@yandex.ru>
2022-12-31 09:36:33 +0300

    note -> PC

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,43 @@
+import Head from 'next/head';
+import Link from 'next/link';
 import React from 'react';
 
-export default function Layout() {
+export default function Layout({ title, children }) {
   return (
     <>
-      <div>
-        <header>header</header>
-        <main>main</main>
+      <Head>
+        <title>{title ? title + '- ElkaBrick.ru' : 'ElkaBrick.ru'}</title>
+        <description>LOL</description>
+        <link
+          rel='icon'
+          href='/favicon.ico'
+        />
+      </Head>
+
+      <div className='flex min-h-screen flex-col justify-between'>
+        <header>
+          <nav>
+            <Link
+              href='/'
+              className='text-lg font-bold'
+            >
+              ElkaBrick
+            </Link>
+            <Link
+              href='/cart'
+              className='text-lg font-bold'
+            >
+              Cart
+            </Link>
+            <Link
+              href='/login'
+              className='text-lg font-bold'
+            >
+              Login
+            </Link>
+          </nav>
+        </header>
+        <main>{children}</main>
         <footer>footer</footer>
       </div>
     </>
